dcab1021153b709924da797b6c7cb39274fd6f64
Soon Wen Hoh <soonwen.hoh@york.ac.uk>
2023-03-02 16:23:15 +0000

    change docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -5,26 +5,28 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
 
-project = 'pysheetbend'
-copyright = '2022, Soon Wen Hoh, Kevin Cowtan'
-author = 'Soon Wen Hoh, Kevin Cowtan'
-release = '0.3.3-alpha'
+project = "pysheetbend"
+copyright = "2022, Soon Wen Hoh, Kevin Cowtan"
+author = "Soon Wen Hoh, Kevin Cowtan"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-source_suffix = ['.rst']
+source_suffix = [".rst"]
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+if not os.environ.get("READTHEDOCS"):
+    html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
